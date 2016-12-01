4d941cacdb07a2f29059bc1bd35675f96833bf1d
Lukas Pfannschmidt <mirek.1337@gmail.com>
2016-12-01 10:21:48 +0100

    remamed template
    Signed-off-by: Lukas Pfannschmidt <mirek.1337@gmail.com>

---
+++ rbclassifier/__init__.py
@@ -0,0 +1,4 @@
+from .rbclassifier import (RelevanceBoundsClassifier)
+from . import rbclassifier
+
+__all__ = ['RelevanceBoundsClassifier', 'rbclassifier']
--- setup.py
+++ setup.py
@@ -18,11 +18,11 @@ except ImportError:
     print('scipy is required during installation')
     sys.exit(1)
 
-setup(name='sklearn-template',
-      version='0.0.3',
-      description='A template for scikit-learn compatible packages',
-      author='Vighnesh Birodkar',
+setup(name='rbclassifier',
+      version='0.1',
+      description='L1 relevance bounds feature classifier',
+      author='Lukas Pfannschmidt',
       packages=find_packages(),
       install_requires=INSTALL_REQUIRES,
-      author_email='vighneshbirodkar@nyu.edu',
+      author_email='lpfannschmidt@techfak.uni-bielefeld.de',
       )
