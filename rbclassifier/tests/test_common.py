04e1ca2b97d147f1ad3f9503d502af971bf97968
Lukas Pfannschmidt <mirek.1337@gmail.com>
2016-12-01 10:24:56 +0100

    renamed tests

--- rbclassifier/tests/test_common.py
+++ rbclassifier/tests/test_common.py
@@ -1,15 +1,9 @@
 from sklearn.utils.estimator_checks import check_estimator
-from skltemplate import (TemplateEstimator, TemplateClassifier,
-                         TemplateTransformer)
+from rbclassifier import (RelevanceBoundsClassifier)
 
-
-def test_estimator():
-    return check_estimator(TemplateEstimator)
 
 
 def test_classifier():
-    return check_estimator(TemplateClassifier)
+    return check_estimator(rbclassifier)
 
 
-def test_transformer():
-    return check_estimator(TemplateTransformer)
