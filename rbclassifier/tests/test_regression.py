d9bb2ce6bd1bd282c6898115e5d82dec526cff5c
Lukas Pfannschmidt <mirek.1337@gmail.com>
2017-01-19 11:43:47 +0100

    used pytest for  automatic parameter tests

--- rbclassifier/tests/test_regression.py
+++ rbclassifier/tests/test_regression.py
@@ -1,5 +1,7 @@
 import unittest
 
+import pytest
+
 import rbclassifier.rbclassifier
 from rbclassifier.rbclassifier import RelevanceBoundsRegressor
 from sklearn.feature_selection import SelectFromModel
@@ -14,130 +16,166 @@ from numpy.testing import assert_array_almost_equal, assert_array_equal,assert_r
 import numpy as np
 from sklearn.exceptions import FitFailedWarning
 from sklearn.preprocessing import StandardScaler
+from  sklearn.exceptions import FitFailedWarning
 
-class TestRegression(unittest.TestCase):
-    def test_simpleRegression1Strong(self):
+from beeprint import pp
 
-        strong = 1
-        weak = 0
-        generator = check_random_state(0)
-        data = rbclassifier.genData.genRegressionData(n_samples=100, n_features=4, n_redundant=weak,strRel=strong,
-                        n_repeated=0, random_state=generator)
+@pytest.fixture(scope="module")
+def randomstate():
+   return check_random_state(0)
 
-        X_orig, y = data
-        X_orig = StandardScaler().fit(X_orig).transform(X_orig)
-        X = X_orig
-        # X = np.c_[X_orig, generator.normal(size=(len(X_orig), 6))]
-        # y = list(y)
+@pytest.fixture(scope="module",
+                params=[(1,0),(2,0),(0,2),(1,2)],
+                ids=["1strong","2strong","weak","allrele"])
+def data(request,randomstate):
+    strong = request.param[0]
+    weak = request.param[1]
+    generator = randomstate
+    data = rbclassifier.genData.genRegressionData(n_samples=1000, n_features=4, n_redundant=weak, strRel=strong,
+                                                  n_repeated=0, random_state=generator)
 
-        # Test using the score function
-        rbc = RelevanceBoundsRegressor(random_state=generator, shadow_features=False)
+    X_orig, y = data
+    X_orig = StandardScaler().fit(X_orig).transform(X_orig)
+    X = X_orig
+    # X = np.c_[X_orig, generator.normal(size=(len(X_orig), 6))]
+    # y = list(y)
+    return X,y,strong,weak
+
+
+def test_simpleRegression(data,randomstate):
+    X, y, strong, weak = data
+
+    # Test using the score function
+    rbc = RelevanceBoundsRegressor(random_state=randomstate, shadow_features=False,C=1)
+    try:
         rbc.fit(X, y)
+    except FitFailedWarning:
+        print(rbc._best_clf_score)
+        return
 
-        assert_equal(len(rbc.allrel_prediction_), X.shape[1])
-        assert_equal(len(rbc.interval_), X.shape[1])
+    assert_equal(len(rbc.allrel_prediction_), X.shape[1])
+    assert_equal(len(rbc.interval_), X.shape[1])
 
-        X_r = rbc.transform(X)
-        print(rbc.interval_,rbc.allrel_prediction_)
+    X_r = rbc.transform(X)
+    print(rbc._best_clf_score)
+    print(rbc.interval_,rbc.allrel_prediction_,rbc._hyper_C,rbc._hyper_epsilon)
 
-        # All the noisy variable were filtered out
-        assert_array_equal(X_r, X_orig)
+    # All strongly relevant features have a lower bound > 0
+    assert np.all(rbc.interval_[0:strong,0] > 0)
+    assert np.all(rbc.interval_[strong:weak,0] == 0)
 
-        # All strongly relevant features have a lower bound > 0
-        assert_true(np.all(rbc.interval_[0:strong,0]>0))
+def test_simple(randomstate):
+    generator = randomstate
+    data = rbclassifier.genData.genRegressionData(n_samples=300, n_features=1, n_redundant=0, strRel=1,
+                                                  n_repeated=0, random_state=generator)
 
-
-    def test_simpleRegression2Strong(self):
-        strong = 2
-        weak = 0
-        generator = check_random_state(0)
-        data = rbclassifier.genData.genRegressionData(n_samples=100, n_features=4, n_redundant=weak, strRel=strong,
-                                                      n_repeated=0, random_state=generator)
-
-        X_orig, y = data
-        X_orig = StandardScaler().fit(X_orig).transform(X_orig)
-        X = X_orig
-        # X = np.c_[X_orig, generator.normal(size=(len(X_orig), 6))]
-        # y = list(y)
-
-        # Test using the score function
-        rbc = RelevanceBoundsRegressor(C=1, random_state=generator, shadow_features=False)
-        rbc.fit(X, y)
-
-        assert_equal(len(rbc.allrel_prediction_), X.shape[1])
-        assert_equal(len(rbc.interval_), X.shape[1])
-
-        X_r = rbc.transform(X)
-        print(rbc.interval_, rbc.allrel_prediction_)
-
-        # All the noisy variable were filtered out
-        assert_array_equal(X_r, X_orig)
-
-        # All strongly relevant features have a lower bound > 0
-        assert_true(np.all(rbc.interval_[0:strong, 0] > 0))
+    X_orig, y = data
+    X_orig = StandardScaler().fit(X_orig).transform(X_orig)
+    X = X_orig
 
 
-    def test_simpleRegressionWeak(self):
-            strong = 0
-            weak = 2
-            generator = check_random_state(0)
-            data = rbclassifier.genData.genRegressionData(n_samples=100, n_features=4, n_redundant=weak,strRel=strong,
-                            n_repeated=0, random_state=generator)
+    # Test using the score function
+    rbc = RelevanceBoundsRegressor(random_state=randomstate, shadow_features=False,C=1)
+    rbc.fit(X, y)
 
-            X_orig, y = data
-            X_orig = StandardScaler().fit(X_orig).transform(X_orig)
-            X = X_orig
-            # X = np.c_[X_orig, generator.normal(size=(len(X_orig), 6))]
-            # y = list(y)
+    assert_equal(len(rbc.allrel_prediction_), X.shape[1])
+    assert_equal(len(rbc.interval_), X.shape[1])
 
-            # Test using the score function
-            rbc = RelevanceBoundsRegressor(C=1,random_state=generator, shadow_features=False)
-            rbc.fit(X, y)
+    X_r = rbc.transform(X)
+    print(rbc.interval_,rbc.allrel_prediction_,rbc._hyper_C,rbc._hyper_epsilon)
+    pp(rbc)
 
-            assert_equal(len(rbc.allrel_prediction_), X.shape[1])
-            assert_equal(len(rbc.interval_), X.shape[1])
+    # All strongly relevant features have a lower bound > 0
+    assert rbc.interval_[0,0] > 0
 
-            X_r = rbc.transform(X)
-            print(rbc.interval_,rbc.allrel_prediction_)
-
-            # All the noisy variable were filtered out
-            assert_array_equal(X_r, X_orig)
-
-            # All strongly relevant features have a lower bound > 0
-            assert_false(np.any(rbc.interval_[0:weak, 0] > 0))
-
-
-    def test_simpleRegressionAllRelevant(self):
-        strong = 1
-        weak = 2
-        generator = check_random_state(0)
-        data = rbclassifier.genData.genRegressionData(n_samples=100, n_features=4, n_redundant=weak, strRel=strong,
-                                                      n_repeated=0, random_state=generator)
-
-        X_orig, y = data
-        X_orig = StandardScaler().fit(X_orig).transform(X_orig)
-        X = X_orig
-        # X = np.c_[X_orig, generator.normal(size=(len(X_orig), 6))]
-        # y = list(y)
-
-        # Test using the score function
-        rbc = RelevanceBoundsRegressor(C=1, random_state=generator, shadow_features=False)
-        rbc.fit(X, y)
-
-        assert_equal(len(rbc.allrel_prediction_), X.shape[1])
-        assert_equal(len(rbc.interval_), X.shape[1])
-
-        X_r = rbc.transform(X)
-        print(rbc.interval_, rbc.allrel_prediction_)
-
-        # All the noisy variable were filtered out
-        assert_array_equal(X_r, X_orig)
-
-        # All strongly relevant features have a lower bound > 0
-        assert_true(np.all(rbc.interval_[0:strong,0]>0))
-        # All weakly relevant features should have a lower bound 0
-        assert_false(np.any(rbc.interval_[strong:weak,0]>0))
-
-
-if __name__ == '__main__':
-    unittest.main()
+# def test_simpleRegression2Strong():
+#     strong = 2
+#     weak = 0
+#     generator = check_random_state(0)
+#     data = rbclassifier.genData.genRegressionData(n_samples=100, n_features=4, n_redundant=weak, strRel=strong,
+#                                                   n_repeated=0, random_state=generator)
+#
+#     X_orig, y = data
+#     X_orig = StandardScaler().fit(X_orig).transform(X_orig)
+#     X = X_orig
+#     # X = np.c_[X_orig, generator.normal(size=(len(X_orig), 6))]
+#     # y = list(y)
+#
+#     # Test using the score function
+#     rbc = RelevanceBoundsRegressor( random_state=generator, shadow_features=False)
+#     rbc.fit(X, y)
+#
+#     assert_equal(len(rbc.allrel_prediction_), X.shape[1])
+#     assert_equal(len(rbc.interval_), X.shape[1])
+#
+#     X_r = rbc.transform(X)
+#     print(rbc.interval_, rbc.allrel_prediction_)
+#
+#     # All the noisy variable were filtered out
+#     assert_array_equal(X_r, X_orig)
+#
+#     # All strongly relevant features have a lower bound > 0
+#     assert_true(np.all(rbc.interval_[0:strong, 0] > 0))
+#
+#
+# def test_simpleRegressionWeak():
+#         strong = 0
+#         weak = 2
+#         generator = check_random_state(0)
+#         data = rbclassifier.genData.genRegressionData(n_samples=100, n_features=4, n_redundant=weak,strRel=strong,
+#                         n_repeated=0, random_state=generator)
+#
+#         X_orig, y = data
+#         X_orig = StandardScaler().fit(X_orig).transform(X_orig)
+#         X = X_orig
+#         # X = np.c_[X_orig, generator.normal(size=(len(X_orig), 6))]
+#         # y = list(y)
+#
+#         # Test using the score function
+#         rbc = RelevanceBoundsRegressor(random_state=generator, shadow_features=False)
+#         rbc.fit(X, y)
+#
+#         assert_equal(len(rbc.allrel_prediction_), X.shape[1])
+#         assert_equal(len(rbc.interval_), X.shape[1])
+#
+#         X_r = rbc.transform(X)
+#         print(rbc.interval_,rbc.allrel_prediction_)
+#
+#         # All the noisy variable were filtered out
+#         assert_array_equal(X_r, X_orig)
+#
+#         # All strongly relevant features have a lower bound > 0
+#         assert_false(np.any(rbc.interval_[0:weak, 0] > 0))
+#
+#
+# def test_simpleRegressionAllRelevant():
+#     strong = 1
+#     weak = 2
+#     generator = check_random_state(0)
+#     data = rbclassifier.genData.genRegressionData(n_samples=100, n_features=4, n_redundant=weak, strRel=strong,
+#                                                   n_repeated=0, random_state=generator)
+#
+#     X_orig, y = data
+#     X_orig = StandardScaler().fit(X_orig).transform(X_orig)
+#     X = X_orig
+#     # X = np.c_[X_orig, generator.normal(size=(len(X_orig), 6))]
+#     # y = list(y)
+#
+#     # Test using the score function
+#     rbc = RelevanceBoundsRegressor(C=1, random_state=generator, shadow_features=False)
+#     rbc.fit(X, y)
+#
+#     assert_equal(len(rbc.allrel_prediction_), X.shape[1])
+#     assert_equal(len(rbc.interval_), X.shape[1])
+#
+#     X_r = rbc.transform(X)
+#     print(rbc.interval_, rbc.allrel_prediction_)
+#
+#     # All the noisy variable were filtered out
+#     assert_array_equal(X_r, X_orig)
+#
+#     # All strongly relevant features have a lower bound > 0
+#     assert_true(np.all(rbc.interval_[0:strong,0]>0))
+#     # All weakly relevant features should have a lower bound 0
+#     assert_false(np.any(rbc.interval_[strong:weak,0]>0))
+#
