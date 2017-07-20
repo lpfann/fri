61c82b874575169d63a251012eacd0330f8215ae
Lukas Pfannschmidt <mirek.1337@gmail.com>
2017-07-20 22:34:48 +0200

    added option for string ticklables in plot

--- rbclassifier/plot.py
+++ rbclassifier/plot.py
@@ -16,35 +16,29 @@ bmap = sns.color_palette("Set2", 5)
 sns.set(style='ticks', palette='Set2')
 sns.despine()
 
-def plotIntervals(ranges):
-	fig = plt.figure(figsize=(13, 6))
-	ax = fig.add_subplot(111)
+def plotIntervals(ranges,ticklables=None):
+    fig = plt.figure(figsize=(13, 6))
+    ax = fig.add_subplot(111)
 
-	N = len(ranges)
+    N = len(ranges)
+    if  ticklables is None:
+        ticks = np.arange(N)+1
+    else:
+        ticks = ticklables
+    
+    ind = np.arange(N)+1
+    width = 0.6
+    upper_vals = ranges[:,1]
+    lower_vals = ranges[:,0]
 
-	ind = np.arange(N)+1
-	width = 0.6
-	upper_vals = ranges[:,1]
-	lower_vals = ranges[:,0]
+    ax.bar(ind, upper_vals - lower_vals, width,bottom=lower_vals,tick_label=ticks,align="center" , edgecolor="black",linewidth=1.3)
+    
+    plt.xticks(ind,ticks, rotation='vertical')
+    # loc = plticker.MultipleLocator(base=1.0) # this locator puts ticks at regular intervals
+    # ax.xaxis.set_major_locator(loc)
+    # ax.set_ylim([0,1])
+    #ax.set_xlim([0,33])
+    plt.ylabel('relevance',fontsize=19)
+    plt.xlabel('feature',fontsize=19)
 
-	ax.bar(ind, upper_vals - lower_vals, width,bottom=lower_vals,tick_label=ind,align="center" , edgecolor="black",linewidth=1.3)
-	# loc = plticker.MultipleLocator(base=1.0) # this locator puts ticks at regular intervals
-	# ax.xaxis.set_major_locator(loc)
-	# ax.set_ylim([0,1])
-	#ax.set_xlim([0,33])
-	plt.ylabel('relevance',fontsize=19)
-	plt.xlabel('feature',fontsize=19)
-	# names = {
-	#     0:"Androgens",
-	#     1:"Androgen precursors",
-	#     2:"Mineralocorticoids",
-	#     3:"Glucocorticoid precursors",
-	#     4:"Glucocorticoids"
-	# }
-
-#	patches = []
-#	for x in names:
-#	    patches.append(mpatches.Patch(color=bmap[x], label=names[x]))
-#	plt.legend(handles=patches,fontsize=14)
-	#plt.show()
-	return fig
+    return fig
