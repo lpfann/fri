3d471671e9bd7f09305b688a02f0592115824509
Lukas Pfannschmidt <lpfannschmidt@techfak.uni-bielefeld.de>
2017-08-14 15:26:27 +0200

    added classes to module init

--- fri/__init__.py
+++ fri/__init__.py
@@ -1,9 +1,9 @@
-from .fri import (FRIClassification)
+from .fri import (FRIClassification,FRIRegression,EnsembleFRI)
 from . import fri
 from . import optproblems
 from . import genData
 from . import bounds
-__all__ = ['FRIClassification', 'FRIRegression','fri.py']
+__all__ = ['FRIClassification', 'FRIRegression','fri.py','EnsembleFRI']
 
 from ._version import get_versions
 __version__ = get_versions()['version']
