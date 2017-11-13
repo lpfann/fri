8dcac884d073d7027400c2175ef7970238e1edbe
Lukas Pfannschmidt <lpfannschmidt@techfak.uni-bielefeld.de>
2017-11-13 16:34:55 +0100

    changed CI configs for better git dependency handling

--- ci_scripts/travis/install.sh
+++ ci_scripts/travis/install.sh
@@ -37,4 +37,4 @@ fi
 python --version
 python -c "import numpy; print('numpy %s' % numpy.__version__)"
 python -c "import scipy; print('scipy %s' % scipy.__version__)"
-python setup.py develop
+python install -e . --process-dependency-links --allow-all-external
