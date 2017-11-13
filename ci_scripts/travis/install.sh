034aa66522dba24c3f03438271076b0ff9d211e7
Lukas Pfannschmidt <lpfannschmidt@techfak.uni-bielefeld.de>
2017-11-13 16:56:49 +0100

    bugfix3

--- ci_scripts/travis/install.sh
+++ ci_scripts/travis/install.sh
@@ -37,4 +37,4 @@ fi
 python --version
 python -c "import numpy; print('numpy %s' % numpy.__version__)"
 python -c "import scipy; print('scipy %s' % scipy.__version__)"
-pip install -r requirements.txt
+python setup.py develop
