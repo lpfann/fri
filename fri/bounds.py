8f916580fc83637281b708b970fd02516913220b
Lukas Pfannschmidt <lpfannschmidt@techfak.uni-bielefeld.de>
2018-02-08 18:24:16 +0100

    added whitespace

--- fri/bounds.py
+++ fri/bounds.py
@@ -75,8 +75,10 @@ class UpperBound(Bound):
         if len(valid_problems) == 0:
             print("DEBUG: Upper Bound - current_feature={}".format(self.di))
             raise NotFeasibleForParameters
+
         max_index = np.argmax([np.abs(x.problem.value) for x in valid_problems])
         best_problem = valid_problems[max_index]
+
         self.prob_instance = best_problem
         return self
 
