77679451b73c62cfe96a533f5a9431112d8c4d8d
Lukas Pfannschmidt <mirek.1337@gmail.com>
2018-02-12 23:37:54 +0100

    test for boolean fs mask methods and # of features

--- fri/tests/test_feature_selection.py
+++ fri/tests/test_feature_selection.py
@@ -1,5 +1,5 @@
 import pytest
-from fri.genData import genData, genRegressionData
+from fri.genData import genData, genRegressionData, genClassificationData
 from fri.fri import FRIRegression,  FRIClassification, EnsembleFRI
 from sklearn.utils import check_random_state
 import numpy as np
@@ -38,7 +38,7 @@ def test_model(problem, model, n_strong, n_weak, randomstate):
         gen = genRegressionData
         fri = FRIRegression(random_state = randomstate, C=1)
     else:
-        gen = genData
+        gen = genClassificationData
         fri = FRIClassification(random_state = randomstate, C=1)
 
     if n_strong + n_weak == 0:
@@ -59,14 +59,18 @@ def test_model(problem, model, n_strong, n_weak, randomstate):
 
         fri.fit(X, y)
 
+        # Check the interval output
         interval = fri.interval_
-
         assert len(fri.allrel_prediction_) == X.shape[1]
         assert len(interval) == X.shape[1]
-
         check_interval(interval, n_strong)
-
+        # Check the score which should be good
         assert fri.score(X[:30], y[:30]) >= 0.8
+        # Check the feature selection methods providing boolean masks and #n of selected features
+        assert fri.n_features_() == n_strong + n_weak
+        truth = np.zeros(n_features)
+        truth[:n_strong+n_weak] = 1
+        assert all(fri._get_support_mask() == truth)
 
 def test_multiprocessing(randomstate):
 
