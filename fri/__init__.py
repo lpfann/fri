8408ca9d4f97d179fbe9b0fba1d04774f9becde0
Lukas Pfannschmidt <lpfannschmidt@techfak.uni-bielefeld.de>
2018-02-12 18:45:59 +0100

    rollback display change no import of plot in init

--- fri/__init__.py
+++ fri/__init__.py
@@ -3,7 +3,6 @@ from . import fri
 from . import optproblems
 from . import genData
 from . import bounds
-from . import plot
 __all__ = ['FRIClassification', 'FRIRegression','fri.py','EnsembleFRI',"plot"]
 
 from ._version import get_versions
