cef08aa099ba1ab117fc9b7bb8d2c1f6c9e677b9
Lukas Pfannschmidt <lpfannschmidt@techfak.uni-bielefeld.de>
2018-02-13 12:35:50 +0100

    better API defínition in module init file

--- fri/__init__.py
+++ fri/__init__.py
@@ -1,10 +1,13 @@
+"""
+ FRI module for inferring relevance intervals for linear classification and regression data
+"""
 from .fri import (FRIClassification,FRIRegression,EnsembleFRI)
-from . import fri
-from . import optproblems
-from . import genData
-from . import bounds
-__all__ = ['FRIClassification', 'FRIRegression','fri.py','EnsembleFRI',"plot"]
+from .genData import genRegressionData, genClassificationData
+from .plot import plotIntervals
+from .plot import plot_dendrogram_and_intervals
+__all__ = ['FRIClassification', 'FRIRegression','EnsembleFRI',"genRegressionData","genClassificationData","plotIntervals"]
 
+# Get version from versioneer
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
