b0f0000187ed8513c3367940e0c25d4ba141d42a
Lukas Pfannschmidt <lpfannschmidt@techfak.uni-bielefeld.de>
2018-02-13 15:12:21 +0100

    added tests for both bound types

--- fri/tests/test_bounds.py
+++ fri/tests/test_bounds.py
@@ -1,3 +1,5 @@
+"""Summary
+"""
 import pytest
 from pytest import approx
 from fri import genClassificationData, genRegressionData
@@ -13,11 +15,20 @@ from sklearn.preprocessing import StandardScaler
 
 @pytest.fixture(scope="function")
 def randomstate():
+   """ pytest fixture providing numpy randomstate 
+   
+   Returns:
+       TYPE: numpy randomstate
+   """
    return check_random_state(1337)
 
 
-def test_bound(randomstate):
-
+def test_bounds(randomstate):
+    """Test bound functions using very easy data with perfect separation
+    
+    Args:
+        randomstate  pytest fixture providing numpy randomstate:
+    """
     n_samples = 4
     n_features = 2
     X = [
@@ -46,44 +57,32 @@ def test_bound(randomstate):
     assert coef[0] == approx(1)
     assert coef[1] == approx(0)
 
-    # 
-    # Lower Bound 
-    # 
-    current_dim = 0
     kwargs = {"verbose":False,"solver":"ECOS","max_iters":1000}
     isRegression = False
     epsilon = None
-    bound = LowerBound(current_dim, n_features,
-                        n_samples, kwargs, L1,
-                        loss, C, X, y,
-                        regression=isRegression,
-                        epsilon=epsilon)
-    bound.solve()
+    for current_dim in range(n_features):
+        for b in (LowerBound,UpperBound):
+                
+            bound = b(current_dim, n_features,
+                                n_samples, kwargs, L1,
+                                loss, C, X, y,
+                                regression=isRegression,
+                                epsilon=epsilon)
+            bound.solve()
+            
+            prob = bound.prob_instance.problem
+            assert prob.status == OPTIMAL
+            assert prob.value - abs(coef[current_dim]) <= 0.1
+            assert bound.prob_instance.loss.value <= max(0.01,loss*1.001) # add a constant factor to handle numerical instabilities
+            assert bound.prob_instance.weight_norm.value <= L1 * 1.00001
 
-    prob = bound.prob_instance.problem
-    assert prob.status == OPTIMAL
-    assert prob.value - abs(coef[0]) <= 0.1
-    assert approx(bound.prob_instance.loss.value) == max(0.01,loss) # we define bigger than zero loss in problem function, need to test this bound
-    assert bound.prob_instance.weight_norm.value <= L1 * 1.00001
 
-    # 
-    # Upper Bound 
-    # 
-    bound = UpperBound(current_dim, n_features,
-                        n_samples, kwargs, L1,
-                        loss, C, X, y,
-                        regression=isRegression,
-                        epsilon=epsilon)
-    bound.solve()
-
-    prob = bound.prob_instance.problem
-    assert prob.status == OPTIMAL
-    assert prob.value - abs(coef[0]) <= 0.1
-    assert bound.prob_instance.loss.value <= max(0.011,loss*1.001) # add a constant factor to handle numerical instabilities
-    assert bound.prob_instance.weight_norm.value <= L1 * 1.00001
-
-def test_bound_twoMiss(randomstate):
-
+def test_bounds_twoMiss(randomstate):
+    """ Test bound functions using data with two missclassifications
+    
+    Args:
+        randomstate pytest fixture providing numpy randomstate:
+    """
     n_samples = 4
     n_features = 2
     X = [
@@ -104,8 +103,8 @@ def test_bound_twoMiss(randomstate):
     bias = l1init.intercept_
     coef = l1init.coef_[0]
     L1 = np.linalg.norm(coef, ord=1)
-    loss = np.abs(l1init.slack).sum()
-
+    loss = np.abs(np.maximum(0, l1init.slack)).sum()
+    
     # Test the init parameters
     assert bias == approx(0)
     assert L1 == approx(0.5)
@@ -114,35 +113,22 @@ def test_bound_twoMiss(randomstate):
     assert coef[0] == approx(0.5)
     assert coef[1] == approx(0)
 
-    current_dim = 0
     kwargs = {"verbose":False,"solver":"ECOS","max_iters":1000}
     isRegression = False
     epsilon = None
-    bound = LowerBound(current_dim, n_features,
-                        n_samples, kwargs, L1,
-                        loss, C, X, y,
-                        regression=isRegression,
-                        epsilon=epsilon)
-    bound.solve()
 
-    prob = bound.prob_instance.problem
-    assert prob.status == OPTIMAL
-    assert prob.value - abs(coef[0]) <= 0.1
-    assert bound.prob_instance.loss.value <= max(0.011,loss*1.001) # add a constant factor to handle numerical instabilities 
-    assert bound.prob_instance.weight_norm.value <= L1 * 1.00001
-
-    # 
-    # Upper Bound 
-    # 
-    bound = UpperBound(current_dim, n_features,
-                        n_samples, kwargs, L1,
-                        loss, C, X, y,
-                        regression=isRegression,
-                        epsilon=epsilon)
-    bound.solve()
-
-    prob = bound.prob_instance.problem
-    assert prob.status == OPTIMAL
-    assert prob.value - abs(coef[0]) <= 0.1
-    assert bound.prob_instance.loss.value <= max(0.011,loss*1.001) # add a constant factor to handle numerical instabilities
-    assert bound.prob_instance.weight_norm.value <= L1 * 1.00001
+    for current_dim in range(n_features):
+        for b in (LowerBound, UpperBound):
+                
+            bound = b(current_dim, n_features,
+                                n_samples, kwargs, L1,
+                                loss, C, X, y,
+                                regression=isRegression,
+                                epsilon=epsilon)
+            bound.solve()
+            
+            prob = bound.prob_instance.problem
+            assert prob.status == OPTIMAL
+            assert prob.value - abs(coef[current_dim]) <= 0.1
+            assert bound.prob_instance.loss.value <= max(0.011,loss*1.001) # add a constant factor to handle numerical instabilities
+            assert bound.prob_instance.weight_norm.value <= L1 * 1.00001
