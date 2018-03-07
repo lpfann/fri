1380a07794e4dfdf92671fa3eeb5988ee5daf584
Jonathan Jakob <jjakob@techfak.uni-bielefeld.de>
2018-03-07 23:34:11 +0100

    deleted epsilon variable in ordinalRegression.py

--- fri/ordinalregression.py
+++ fri/ordinalregression.py
@@ -17,11 +17,9 @@ class FRIOrdinalRegression(FRIBase):
 
     def __init__(self, epsilon=None, C=None, random_state=None,
                  shadow_features=False, parallel=False, feat_elim=False, **kwargs):
-        super().__init__(isRegression=True, C=C, random_state=random_state,
+        super().__init__(isRegression=False, C=C, random_state=random_state,
                          shadow_features=shadow_features, parallel=parallel, feat_elim=False, **kwargs)
 
-        # epsilon is the delta from the theory paper
-        self.epsilon = epsilon
         self.initModel = L1OrdinalRegressor
 
         # Define parameters which are optimized in the initial gridsearch
@@ -33,11 +31,6 @@ class FRIOrdinalRegression(FRIBase):
             self.tuned_parameters["C"] = [0.0001, 0.001, 0.01, 0.1, 1, 10, 100]
         else:
             self.tuned_parameters["C"] = [self.C]
-
-        if self.epsilon is None:
-            self.tuned_parameters["epsilon"] = [0, 0.0001, 0.001, 0.01, 0.1, 1, 10, 100, 1000]
-        else:
-            self.tuned_parameters["epsilon"] = [self.epsilon]
 
 
     def fit(self, X, y):
