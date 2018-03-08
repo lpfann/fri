88f3b3e0b7998d9876925c98895bfc9e36f78456
Jonathan Jakob <jjakob@techfak.uni-bielefeld.de>
2018-03-08 15:06:04 +0100

    implemented optProblems

--- fri/optproblems.py
+++ fri/optproblems.py
@@ -1,6 +1,7 @@
 from abc import ABCMeta
 
 import cvxpy as cvx
+import numpy as np
 
 #
 # Minimal loss in constraints to mitigate numerical instabilities for solvers
@@ -207,20 +208,91 @@ class BaseOrdinalRegressionProblem(BaseProblem):
     __metaclass__ = ABCMeta
     def __init__(self, di=None, kwargs=None, X=None, Y=None,  initLoss=None, initL1=None, parameters=None):
         super().__init__(di=di, kwargs=kwargs, X=X, Y=Y, initLoss=initLoss, initL1=initL1,parameters=parameters)
-        pass
+
+        # Solver parameters
+        self.kwargs = kwargs
+
+        # Optimal parameters from initial gridsearch
+        self.C = self.parameters["C"]
+        self.w_opt = self.parameters["w"]
+        self.chi_opt = self.parameters["chi"]
+        self.xi_opt = self.parameters["xi"]
+
+        #TODO: Set appropriate delta value
+        self.delta = 0.1
+        self.mu = np.linalg.norm(self.w_opt, ord=1) + self.C * np.sum(self.chi_opt + self.xi_opt)
+
+        # Prepare the same Problem structure as in initial search
+        (n, d) = X.shape
+        n_bins = len(np.unique(Y))
+
+        X_re = []
+        y = np.array(Y)
+        for i in range(n_bins):
+            indices = np.where(y == i)
+            X_re.append(X[indices])
+
+
+        self.w = cvx.Variable(shape=(d, 1))
+        self.b = cvx.Variable(shape=(n_bins - 1, 1))
+        self.chi = []
+        self.xi = []
+        for i in range(n_bins):
+            n_x = len(np.where(y == i)[0])
+            self.chi.append(cvx.Variable(shape=(n_x, 1)))
+            self.xi.append(cvx.Variable(shape=(n_x, 1)))
+
+
+        self._constraints = []
+        for i in range(n_bins - 1):
+            self._constraints.append(X_re[i] * self.w - self.chi[i] <= self.b[i] - 1)
+
+        for i in range(1, n_bins):
+            self._constraints.append(X_re[i] * self.w + self.xi[i] >= self.b[i - 1] + 1)
+
+        for i in range(n_bins - 2):
+            self._constraints.append(self.b[i] <= self.b[i + 1])
+
+        for i in range(n_bins):
+            self._constraints.append(self.chi[i] >= 0)
+            self._constraints.append(self.xi[i] >= 0)
+
+        # Extend contstraints with regard to the initial problem
+        self._constraints.append(cvx.norm(self.w,1) + self.C * cvx.sum(cvx.hstack(self.chi) + cvx.hstack(self.xi)) <= (1 + self.delta) * self.mu)
+        self._constraints.append(self.w <= cvx.abs(self.w))
+        self._constraints.append(-self.w <= cvx.abs(self.w))
+
+
+
 
 
 class MinProblemOrdinalRegression(BaseOrdinalRegressionProblem):
     def __init__(self, di=None, kwargs=None, X=None, Y=None, initLoss=None, initL1=None, parameters=None):
         super().__init__(di=di, kwargs=kwargs, X=X, Y=Y, initLoss=initLoss, initL1=initL1, parameters=parameters)
-        pass
+
+        # Define problem specific objective
+        self._objective = cvx.Minimize(cvx.abs(self.w[self.di]))
+
+
 
 class MaxProblem1OrdinalRegression(BaseOrdinalRegressionProblem):
     def __init__(self, di=None, kwargs=None, X=None, Y=None, epsilon=None, initLoss=None, initL1=None, parameters=None):
         super().__init__(di=di, kwargs=kwargs, X=X, Y=Y, initLoss=initLoss, initL1=initL1, parameters=parameters)
-        pass
+
+        # Extend problem specific constraints
+        self._constraints.append(cvx.abs(self.w[self.di]) <= self.w[self.di])
+
+        # Define problem specific objective
+        self._objective = cvx.Maximize(cvx.abs(self.w[self.di]))
+
+
 
 class MaxProblem2OrdinalRegression(BaseOrdinalRegressionProblem):
     def __init__(self, di=None, kwargs=None, X=None, Y=None, epsilon=None, initLoss=None, initL1=None, parameters=None):
         super().__init__(di=di, kwargs=kwargs, X=X, Y=Y, initLoss=initLoss, initL1=initL1, parameters=parameters)
-        pass
+
+        # Extend problem specific constraints
+        self._constraints.append(cvx.abs(self.w[self.di]) <= -self.w[self.di])
+
+        # Define problem specific objective
+        self._objective = cvx.Maximize(cvx.abs(self.w[self.di]))
