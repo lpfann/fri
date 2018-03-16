494210026d11375e1b451a1bd24b02f7cee28fa0
Lukas Pfannschmidt <lukas@lpfann.me>
2018-03-16 16:43:33 +0100

    Added ordinal regression to module import

--- fri/__init__.py
+++ fri/__init__.py
@@ -1,15 +1,16 @@
 """
  FRI module for inferring relevance intervals for linear classification and regression data
 """
-from .ensemble import EnsembleFRI
-from .regression import FRIRegression
 from .classification import FRIClassification
+from .ensemble import EnsembleFRI
 from .genData import genRegressionData, genClassificationData
+from .ordinalregression import FRIOrdinalRegression
 from .plot import plotIntervals
 from .plot import plot_dendrogram_and_intervals
+from .regression import FRIRegression
 
 __all__ = ["genRegressionData", "genClassificationData",
-           "plotIntervals","EnsembleFRI","FRIClassification","FRIRegression"]
+           "plotIntervals", "EnsembleFRI", "FRIClassification", "FRIRegression", "FRIOrdinalRegression"]
 
 # Get version from versioneer
 from ._version import get_versions
