f6c9f711adf647a5bc25576518c993cbec456f28
Lukas Pfannschmidt <lukas@lpfann.me>
2018-04-15 14:36:31 +0200

    changed dendgrogram plot: using plotfunction for relevance ranges

--- fri/plot.py
+++ fri/plot.py
@@ -9,7 +9,7 @@ from scipy.cluster.hierarchy import dendrogram
 color_palette_3 = sns.color_palette(palette="muted", n_colors=3)
 
 
-def plot_relevance_bars(ax: matplotlib.axes.Axes, ranges, ticklabels=None, classes=None):
+def plot_relevance_bars(ax: matplotlib.axes.Axes, ranges, ticklabels=None, classes=None, numbering=True):
     N = len(ranges)
 
     # Ticklabels
@@ -17,8 +17,9 @@ def plot_relevance_bars(ax: matplotlib.axes.Axes, ranges, ticklabels=None, class
         ticks = np.arange(N) + 1
     else:
         ticks = list(ticklabels)
-        for i in range(N):
-            ticks[i] += " - {}".format(i + 1)
+        if numbering:
+            for i in range(N):
+                ticks[i] += " - {}".format(i + 1)
 
     # Interval sizes
     ind = np.arange(N) + 1
@@ -45,7 +46,7 @@ def plot_relevance_bars(ax: matplotlib.axes.Axes, ranges, ticklabels=None, class
         ax.set_xticklabels(ax.get_xticklabels(), rotation=30, ha="right")
     # ax.tick_params(rotation="auto")
     # Limit the y range to 0,1 or 0,L1
-    ax.set_ylim([0, 1])
+    ax.set_ylim([0, max(ranges[:, 1]) * 1.1])
 
     ax.set_ylabel('relevance', fontsize=19)
     ax.set_xlabel('feature', fontsize=19)
@@ -75,16 +76,13 @@ def plotIntervals(ranges, ticklabels=None, invert=False, classes=None):
     return fig
 
 
-def plot_dendrogram_and_intervals(intervals, linkage, threshold=0.55, figsize=(13, 7), ticklabels=None, **kwargs):
-    import matplotlib.pyplot as plt
-
-    z = linkage
-    fig = plt.figure(figsize=figsize)
+def plot_dendrogram_and_intervals(intervals, linkage, threshold=0.55, figsize=(13, 7), ticklabels=None, classes=None,
+                                  **kwargs):
+    fig, (ax2, ax) = plt.subplots(2, 1, figsize=figsize)
 
     # Top dendrogram plot
-    ax2 = fig.add_subplot(211)
     d = dendrogram(
-        z,
+        linkage,
         color_threshold=threshold,
         leaf_rotation=0.,  # rotates the x axis labels
         leaf_font_size=12.,  # font size for the x axis labels
@@ -95,30 +93,19 @@ def plot_dendrogram_and_intervals(intervals, linkage, threshold=0.55, figsize=(1
     rearranged_index = d['leaves']
     ranges = intervals[rearranged_index]
 
-    ax = fig.add_subplot(212)
-    N = len(ranges)
     if ticklabels is None:
         ticks = np.array(rearranged_index)
         ticks += 1  # Index starting at 1
     else:
         ticks = list(ticklabels[rearranged_index])
-        for i in range(N):
+        for i in range(len(intervals)):
             ticks[i] += " - {}".format(rearranged_index[i] + 1)
 
-    ind = np.arange(N) + 1
-    width = 0.6
-    upper_vals = ranges[:, 1]
-    lower_vals = ranges[:, 0]
-    bars = ax.bar(ind, upper_vals - lower_vals, width, bottom=lower_vals, tick_label=ticks, align="center",
-                  linewidth=1.3)
+    plot_relevance_bars(ax, ranges, ticklabels=ticks, classes=classes[rearranged_index], numbering=False)
 
-    plt.ylabel('relevance', fontsize=19)
-    plt.xlabel('feature', fontsize=19)
-    plt.xticks(ind, ticks, rotation='30', ha="right")
     ax.margins(x=0)
     ax2.set_xticks([])
     ax2.margins(x=0)
     plt.tight_layout()
-    # plt.subplots_adjust(wspace=0, hspace=0)
 
     return fig
