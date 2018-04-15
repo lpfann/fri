fe78738f9a1495e15dd466bbc72b8ff7b1fe4c2b
Lukas Pfannschmidt <lukas@lpfann.me>
2018-04-15 12:44:14 +0200

    not working for non classification l1 models

--- fri/base.py
+++ fri/base.py
@@ -580,7 +580,7 @@ class FRIBase(BaseEstimator, SelectorMixin):
 
     def score(self, X, y):
         if self.optim_model_:
-            return self.optim_model_.score(X, y, debug=self.debug)
+            return self.optim_model_.score(X, y)
         else:
             raise NotFittedError()
 
