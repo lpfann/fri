acc971f2f05589d2115cf59b3333a866103779bf
Lukas Pfannschmidt <lukas@lpfann.me>
2018-08-15 17:55:44 -0700

    removed doc autosummary

--- doc/conf.py
+++ doc/conf.py
@@ -39,7 +39,6 @@ except:
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
     'sphinx.ext.doctest',
     'sphinx.ext.intersphinx',
     'sphinx.ext.todo',
@@ -50,8 +49,6 @@ extensions = [
 
 ]
 
-# generate autosummary even if no references
-autosummary_generate = False
 
 # pngmath / imgmath compatibility layer for different sphinx versions
 import sphinx
@@ -172,7 +169,7 @@ html_short_title = "fri"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+#html_static_path = ['_static']
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
