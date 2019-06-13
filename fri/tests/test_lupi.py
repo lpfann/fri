622633f61be6f45e0261aa0bb70ee53ab87392b7
Lukas Pfannschmidt <mirek.1337@gmail.com>
2019-06-13 17:40:24 +0200

    Added lupi ordreg parameterized testing method

--- fri/tests/test_lupi.py
+++ fri/tests/test_lupi.py
@@ -178,3 +178,47 @@ def test_strongly_relevant_ordregression(randomstate):
     print(f.interval_)
     assert f.interval_[0, 0] > 0, "Normal SRel feature lower bound error"
     assert f.interval_[1, 0] > 0, "Priv SRel feature lower bound error"
+
+
+@pytest.mark.parametrize('n_strong', [5, 10])
+@pytest.mark.parametrize('n_weak', [0, 2])
+@pytest.mark.parametrize('n_priv_strong', [1, 2])
+@pytest.mark.parametrize('n_priv_weak', [0, 2])
+def test_lupi_model_ord_regression(n_strong, n_weak, n_priv_strong, n_priv_weak, randomstate):
+    n_samples = 500
+    n_features = max(8, n_strong + n_weak)
+
+    gen = genOrdinalRegressionData
+    model = FRI(fri.ProblemType.LUPI_ORDREGRESSION, random_state=randomstate, verbose=1, n_param_search=50)
+
+    n_priv_features = n_priv_strong + n_priv_weak
+    data = genLupiData(gen, n_priv_strel=n_priv_strong, n_priv_redundant=n_priv_weak,
+                       n_priv_features=n_priv_features,
+                       n_samples=n_samples, n_features=n_features, n_redundant=n_weak, n_strel=n_strong,
+                       n_repeated=0, random_state=randomstate
+                       )
+
+    X, X_priv, y = data
+    X = StandardScaler().fit(X).transform(X)
+    X_priv = StandardScaler().fit(X_priv).transform(X_priv)
+    combined = np.hstack([X, X_priv])
+
+    model.fit(combined, y, lupi_features=n_priv_features)
+
+    # Check the interval output
+    interval = model.interval_
+    print(interval)
+    assert len(model.allrel_prediction_) == X.shape[1] + X_priv.shape[1]
+    assert len(interval) == X.shape[1] + X_priv.shape[1]
+
+    n_f = n_strong + n_weak + n_priv_strong + n_priv_weak  # Number of relevant features
+
+    # Check how many are selected
+    selected = model._n_features()
+    print(model._get_support_mask())
+    assert n_f == selected or selected == n_f + 1, "Feature Selection not accurate"
+
+    # Check if all relevant features are selected
+    # truth = np.ones(n_f)
+    # TODO: rework truth vector check for lupi_features test
+    # assert all(fri._get_support_mask()[:n_f] == truth)
