e1704ab8e976911ed4d52d02a74d28d0ded37a6a
Lukas Pfannschmidt <mirek.1337@gmail.com>
2019-08-23 16:08:24 +0200

    Removed implicit ordinal regression variants

--- fri/model/__init__.py
+++ fri/model/__init__.py
@@ -1,6 +1,6 @@
 from .classification import Classification
 from .lupi_classification import LUPI_Classification
-from .lupi_ordinal_regression_exp import LUPI_OrdinalRegression
+from .lupi_ordinal_regression import LUPI_OrdinalRegression
 from .lupi_regression import LUPI_Regression
 from .ordinal_regression import OrdinalRegression
 from .regression import Regression
