Fixed noise scaling in gen data

fri/toydata/gen_data.py
@@ -7,7 +7,7 @@ from sklearn.utils import shuffle
 def _combFeat(n, size, strRelFeat, randomstate):
     # Split each strongly relevant feature into linear combination of it
     weakFeats = np.tile(strRelFeat, (size, 1)).T
-    weakFeats = randomstate.normal(loc=0, scale=1, size=size) + weakFeats
+    weakFeats = randomstate.uniform(low=1, high=2, size=size) + weakFeats
     return weakFeats
 
 
@@ -67,16 +67,19 @@ def _fillVariableSpace(
 
     if partition is not None:
         assert n_redundant == np.sum(partition)
+
     # Create dummy array
     X = np.zeros((int(n_samples), int(n_features)))
+
     # Add strongly relevant
     X[:, :n_strel] = X_informative[:, :n_strel]
+
     # Save strongly relevant used in creation of weakly ones
     holdout = X_informative[:, n_strel:]
 
     i = n_strel
     pi = 0
-    for x in range(len(holdout.T)):
+    for x in range(holdout.shape[1]):
         size = partition[pi]
         X[:, i : i + size] = _combFeat(n_samples, size, holdout[:, x], random_state)
         i += size
@@ -201,7 +204,7 @@ def genClassificationData(
         Y[random_state.choice(n_samples, n_flip)] *= -1
 
     # Add gaussian noise to data
-    X = X + random_state.normal(size=(n_samples, n_features), scale=noise)
+    X = X + random_state.normal(size=(n_samples, n_features), scale=noise / X.std())
 
     return X, Y
 

fri/toydata/gen_lupi.py
@@ -131,7 +131,6 @@ def genLupiData(
     problemName: ProblemName,
     n_samples: int = 100,
     random_state: object = None,
-    noise: float = 0.1,
     n_ordinal_bins: int = 3,
     n_strel: int = 1,
     n_weakrel: int = 0,
@@ -150,8 +149,6 @@ def genLupiData(
                 Number of samples
             random_state : object, optional
                 Randomstate object used for generation.
-            noise : float, optional
-                Noise of the created samples around ground truth.
             n_ordinal_bins : int, optional
                 Number of bins in which the regressional target variable is split to form the ordinal classes,
                 Only has an effect if problemType == 'ordinalRegression'
@@ -181,10 +178,9 @@ def genLupiData(
 
 
         """
-    gr_n = 2  # Group size
 
     random_state = check_random_state(random_state)
-    n_informative = n_strel + int(n_weakrel / gr_n)
+    n_informative = n_strel + (n_weakrel > 0)
 
     # Create truth (prototype) vector which contains true feature contributions
     # We enforce minimum of 0.1 to circumvent problems when testing for relevance
@@ -192,7 +188,7 @@ def genLupiData(
     X_informative = random_state.normal(size=(n_samples, n_informative))
     scores = np.dot(X_informative, w)
 
-    n_features = n_strel + n_weakrel + n_repeated
+    n_features = n_strel + n_weakrel + n_repeated + n_irrel
     X_priv = _fillVariableSpace(
         X_informative,
         random_state,
@@ -202,8 +198,10 @@ def genLupiData(
         n_repeated=n_repeated,
         partition=[n_weakrel],
     )
-
-    e = random_state.normal(size=(n_samples, X_priv.shape[1]), scale=noise)
+    NOISE = 0.65
+    e = random_state.normal(
+        size=(n_samples, X_priv.shape[1]), scale=NOISE / X_priv.std()
+    )
     X = X_priv + e
 
     if (