e0965bceacfc6779962f78d8cd971a734f5d6073
Lukas Pfannschmidt <lukas@lpfann.me>
2020-06-29 10:11:25 +0200

    use dynamic version from package metadata using backported importlib_metadata library required for python < 3.8. lib is included as importlib.metadata in 3.8

--- fri/__init__.py
+++ fri/__init__.py
@@ -1,7 +1,11 @@
 import logging
 
-# noinspection PyUnresolvedReferences
-from fri._version import __version__
+from importlib.metadata import version
+
+try:
+    __version__ = version(__name__)
+except:
+    pass
 
 logging.basicConfig(level=logging.INFO)
 from enum import Enum
